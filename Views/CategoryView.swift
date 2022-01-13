d583b5f406add4cde350e5312ac2ccf87b913eff
mansah82 <manne.sahlin@iths.se>
2022-01-13 15:32:34 +0100

    Trying to solve category

--- Views/CategoryView.swift
+++ Views/CategoryView.swift
@@ -8,95 +8,88 @@
 import SwiftUI
 import Firebase
 
+
 struct CategoryView: View {
     
     var db = Firestore.firestore()
     @State var shoes = [Shoe]()
+    
+    
+    
     //@State var brandLogos = [BrandLogo]()
     
     var body: some View {
+        
         NavigationView {
             VStack {
+                
                 List {
                     ForEach(shoes) { shoe in
+                        
                         HStack {
                             // Problem att logon visas fler gånger
                             NavigationLink(destination: ShoeCard(shoeInfo: shoe)) {
+                                
                                 AsyncImage(url: URL(string: shoe.brandlogo)) { image in
                                     image
                                         .resizable()
                                         .scaledToFit()
                                     
-                                    
                                 } placeholder: {
                                     Image(systemName: "photo")
                                 }
-                                
-                                
-                                
                             }
-                            
-                        }
-                        
-                        
                     }
-                    
-                    .onAppear() {
-                        listenToFireStore()
-                    }
-                    .padding(.vertical)
-                    
-                }
-                .padding(.vertical)
-                .navigationTitle("Brands")
             }
+    }
+        .onAppear() {
+            listenToFireStore()
+}
+        .padding(.vertical)
+    }
+            .padding(.vertical)
+            .navigationTitle("Brands")
+}
             .navigationViewStyle(.stack)
-        }
+        
     }
     
-    
     func listenToFireStore() {
+    
+    db.collection("Shoes").addSnapshotListener { snapshot, err in
+    guard let snapshot = snapshot else {return}
+            
+        if let err = err {
         
-        db.collection("Shoes").addSnapshotListener { snapshot, err in
-            
-            guard let snapshot = snapshot else {return}
-            
-            if let err = err {
-                print("Could not find document: \(err)")
+        print("Could not find document: \(err)")
                 
             } else {
                 shoes.removeAll()
-                
                 for document in snapshot.documents {
-                    let result = Result {
-                        try document.data(as: Shoe.self)
-                        
-                    }
+                let result = Result {
+                try document.data(as: Shoe.self)
+            }
                     switch result {
                     case.success(let shoe):
-                        if let shoe = shoe {
-                            print("Shoe: \(shoe)")
-                            shoes.append(shoe)
-                        } else {
-                            print("Document does not exist")
-                        }
-                    case.failure(let error):
-                        print("Error decoding shoe \(error)")
-                    }
-                }
-                
-                
+                        
+            if let shoe = shoe {
+                print("Shoe: \(shoe)")
+                shoes.append(shoe)
+                            
+            } else {
+                print("Document does not exist")
             }
-            
+                case.failure(let error):
+                print("Error decoding shoe \(error)")
+            }
         }
-        
-        
+    }
     }
 }
-
+}
 
 struct CategoryView_Previews: PreviewProvider {
     static var previews: some View {
-        CategoryView()
-    }
+    CategoryView()
+        }
 }
