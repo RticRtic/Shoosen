98860978e9d665169fb6afb1a1c025a3bb380d0c
mansah82 <manne.sahlin@iths.se>
2022-01-20 14:04:39 +0100

    New sellshoe functions etc

--- Views/SellShoeView.swift
+++ Views/SellShoeView.swift
@@ -8,37 +8,110 @@
 import SwiftUI
 import Firebase
 
+
+
 struct SellShoeView: View {
+    
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerDisplay = false
+    var db = Firestore.firestore()
+    
+    
+    
+    @State var brandInput: String = ""
+    @State var sizeInput : Int = 0
+    @State var colorInput : String = ""
+    @State var priceInput : Int = 0
+    @State var shoetypeInput : String = ""
+    
+    
+    
+    @State var storageManager = StorageManager()
+    
     var body: some View {
-        NavigationView {
-            
-            VStack {
-                Text("Sell shoe/s")
+        NavigationView{
+            List{
+            VStack{
+                Spacer()
+                TextField("Wich brand is it?", text: $brandInput)
+                TextField("What type of shoe is it?", text: $shoetypeInput)
+                TextField("Wich size are they?", value: $sizeInput, formatter: NumberFormatter())
+                TextField("Wich color are they?", text: $colorInput)
+                TextField("Wich price?", value: $priceInput, formatter: NumberFormatter())
+                Button(action:{addToFireStore()}, label: {Text("Save")})
+                Spacer()
+                
+                if selectedImage != nil {
+                    Image(uiImage: selectedImage!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300, height: 300)
+                        .onChange(of: selectedImage!, perform: { image in
+                            storageManager.upload(image: image)
+                           
+                        })
+                } else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 300, height: 300)
+                    
+                }
+                HStack{
+                Button("Camera"){
+                    self.sourceType = .camera
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+                
+                Button("photo"){
+                    self.sourceType = .photoLibrary
+                    self.isImagePickerDisplay.toggle()
+                }.padding()
+                }
+            }
             }
             
-            Button {
-                saveToFireBase()
-            } label: {
-                Text("Save")
+                
+            
+            .navigationBarTitle("Sell your shoe")
+            .sheet(isPresented: self.$isImagePickerDisplay) {
+                ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
             }
-            
-            .navigationTitle("Sell Shoe/s")
-            
         }
-        .navigationViewStyle(.stack)
     }
-    
-    
-    
-    
-    func saveToFireBase() {
-        //db.collection("Shoes").addDocument(data: ["brand" : "adidas"])
+    func addToFireStore(){
         
-    }
+        let shoes = Shoe(id: "" , brand: brandInput.lowercased(), color: colorInput.lowercased(), shoetype: shoetypeInput.lowercased(), price: priceInput, size: sizeInput, image: "", brandlogo: "", showshoe: true)
+        
+        do {
+              let _ = try db.collection("Shoes").addDocument(from: shoes)
+            }
+            catch {
+              print(error)
+            }
+        }
+        
 }
 
 struct SellShoeView_Previews: PreviewProvider {
     static var previews: some View {
         SellShoeView()
     }
+}
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker : ImagePickerView
+    
+    init(picker: ImagePickerView){
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else {return}
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+    
+    
 }
---
+++ Views/StorageManager.swift
@@ -0,0 +1,78 @@
+//
+//  StorageManager.swift
+//  Shoosen
+//
+//  Created by Manne Sahlin on 2022-01-19.
+//
+
+import Foundation
+import SwiftUI
+import Firebase
+
+public class StorageManager: ObservableObject{
+    let storage = Storage.storage()
+    
+    
+    func upload(image : UIImage){
+        
+        let id = UUID()
+        
+        let storageRef = storage.reference().child("images/\(id).jpg")
+        
+        let reziedImage = image
+        
+        let data = reziedImage.jpegData(compressionQuality: 0.2)
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpg"
+        
+        if let data = data{
+            storageRef.putData(data, metadata: metadata) { (metadata, error) in
+                if let error = error {
+                    print("Error while uploading file:", error)
+                }
+                if let metadata = metadata{
+                    print("Metadata:", metadata)
+                }
+            }
+        }
+    }
+
+    func listAllFiles(){
+        let storageRef = storage.reference().child("images")
+        
+        storageRef.listAll{ (result, error) in
+            if let error = error {
+                print("Error while listing all files", error)
+            }
+            for item in result.items {
+                print("Item in images folder", item)
+            }
+        }
+    }
+ 
+    
+    func listItem(){
+        
+        let storageRef = storage.reference().child("images")
+        
+        let handler : (StorageListResult, Error?) -> Void = {(result, error) in
+            if let error = error{
+                print("error", error)
+            }
+            
+            let item = result.items
+            print("item: ", item )
+        }
+    
+        storageRef.list(maxResults: 1, completion: handler)
+    }
+    
+    func deleteItem(item: StorageReference){
+        item.delete { error in
+            if let error = error{
+                print("Error deleting item", error)
+        }
+    }
+}
+}
