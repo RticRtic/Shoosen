fd6570ae7091ca1f9747765e76695674c50d377d
RticRtic <90681250+RticRtic@users.noreply.github.com>
2022-02-09 13:49:04 +0100

    updated favoriteView with add and delete

--- Model/MailView.swift
+++ Model/MailView.swift
@@ -10,12 +10,45 @@ import MessageUI
 import SwiftUI
 import UIKit
 
-struct MailView: UIViewControllerRepresentable {
+
     
-    @EnvironmentObject var presentation
-    @Binding var result: Result<MFMailComposeResult, Error>?
+    //@EnvironmentObject var presentation
+    //@Binding var result: Result<MFMailComposeResult, Error>?
     
-}
+    
+    class Coorinator: NSObject, MFMailComposeViewControllerDelegate {
+        @Binding var presentation: PresentationMode
+        @Binding var result: Result<MFMailComposeResult, Error>?
+        
+        init(presentation: Binding <PresentationMode>,
+             result: Binding<Result<MFMailComposeResult, Error>?>) {
+            _presentation = presentation
+            _result = result
+            
+        }
+        
+        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+            defer {
+                $presentation.wrappedValue.dismiss()
+                
+            }
+            guard error == nil else {
+                self.result = .failure(error!)
+                return
+            }
+            self.result = .success(result)
+        }
+        
+        func makeCoordinator() -> Coordinator {
+            
+        }
+        
+        
+        
+        
+    }
+    
+
 
 
 
